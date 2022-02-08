da24bba8fffb93fe445d96a80a8439e03005414b
Parth Darji <pdarji8825@conestogac.on.ca>
2022-02-08 15:13:32 -0500

    Completed Console Application

--- Asssignment02PD/Program.cs
+++ Asssignment02PD/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Name: Parth Vinodbhai Darji
+ * Course: Software Quality Assurance
+ * Purpose: Unit testing
+ * Revision History
+ *      Created at Feb 08, 2022 by Parth Darji
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,101 @@ namespace Asssignment02PD
     {
         static void Main(string[] args)
         {
+            int length;
+            int width;
+            int selectMenu;
+            bool loop = true;
+            Console.Write("\n\n");
+            Console.Write("--------------------------------------------------------\n");
+            Console.Write("This Program is to get Area and Perimeter for Rectangle size\n");
+            Console.Write("--------------------------------------------------------\n");
+            Console.Write("\n\n");
+
+
+            Console.Write("Please enter the length: ");
+            while (!int.TryParse(Console.ReadLine(), out length) || length < 0)
+            {
+                Console.WriteLine("Please Enter a valid positive integer value!");
+                Console.Write("Please enter the length: ");
+
+            }
+
+            Console.Write("Please enter the width: ");
+            while (!int.TryParse(Console.ReadLine(), out width) || width < 0)
+            {
+                Console.WriteLine("Please Enter a valid positive integer value!");
+                Console.Write("Please enter the width: ");
+
+            }
+            Console.Clear();
+
+            
+            Rectangle rect = new Rectangle(length, width);
+            while (loop)
+            {
+                Console.Write("\n\n");
+                Console.Write("1. Get Rectangle Length\n" +
+                "2. Change Rectangle Length\n" +
+                "3. Get Reactang Width\n" +
+                "4. Change Rectangle Width\n" +
+                "5. Get Rectangle Perimeter\n" +
+                "6. Get Rectangle Area\n" +
+                "7. Exit\n" +
+                "Please SELECT the NUMBER from the above MENU: ");
+                Console.WriteLine();
+                while (!int.TryParse(Console.ReadLine(), out selectMenu))
+                {
+                    Console.WriteLine("Please Enter a valid integer value!");
+                    Console.Write("Please SELECT the NUMBER from the above MENU: ");
+                }
+                switch (selectMenu)
+                {
+                    case 1:
+                        Console.WriteLine($"Length is: {rect.GetLength()}");
+                        break;
+                    case 2:
+                        Console.Write("Please enter new length: ");
+                        while (!int.TryParse(Console.ReadLine(), out length))
+                        {
+                            Console.WriteLine("Please Enter a valid integer value!");
+                            Console.Write("Please enter new length: ");
+                        }
+                        rect.SetLength(length);
+                        break;
+                    case 3:
+                        Console.WriteLine($"Length is: {rect.GetWidth()}");
+                        break;
+                    case 4:
+                        Console.Write("Please enter new width: ");
+                        while (!int.TryParse(Console.ReadLine(), out width))
+                        {
+                            Console.WriteLine("Please Enter a valid integer value!");
+                            Console.Write("Please enter new width: ");
+                        }
+                        rect.SetLength(width);
+                        break;
+                    case 5:
+                        int perimeterResult;
+                        perimeterResult = rect.GetPerimeter();
+                        Console.WriteLine($"Rectangle Perimeter is: {perimeterResult}");
+                        break;
+                    case 6:
+                        int areaResult;
+                        areaResult = rect.GetArea();
+                        Console.WriteLine($"Rectangle Area is: {areaResult}");
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        //To display invalid options
+                        Console.WriteLine();
+                        Console.WriteLine("\" " + selectMenu + " \" is not in the Menu option. Please select between 1 to 7");
+                        break;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- Asssignment02PD/Rectangle.cs
+++ Asssignment02PD/Rectangle.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Asssignment02PD
 {
-    class Rectangle
+    public class Rectangle
     {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int _length, int _width)
+        {
+            this.length = _length;
+            this.width = _width;
+
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+        public int SetLength(int _length)
+        {
+            this.length = _length;
+            return this.length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int SetWidth(int _width)
+        {
+            this.width = _width;
+            return this.width;
+        }
+        public int GetPerimeter()
+        {
+            return (length + width) * 2;
+        }
+        public int GetArea()
+        {
+            return length * width;
+        }
     }
 }
